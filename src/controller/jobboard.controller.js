2a6e033188a360dfd20541ba1adb6dae02896a78
toxirturopov578@gmail.com <toxir>
2025-03-26 10:59:47 -0700

    crud tugadi

--- src/controller/jobboard.controller.js
+++ src/controller/jobboard.controller.js
@@ -6,7 +6,95 @@ const getAllJobs = async (req,res) => {
         message: "success",
         data: jobs
     })
+};
+
+const getOneJobs = async (req,res) => {
+    const { id } = req.params;
+
+    if(!(id)){
+        return res.status(400).send({
+            message: `Given id: ${id} is not valid`
+        });
+    };
+
+    const job = await jobModel.findById(id);
+
+    if(!job){
+        return res.status(404).send({
+            message: `Job with id: ${id} not found`
+        });
+    };
+    
+    res.send({
+        message: "success",
+        data: job
+    });
 }
 
+const createJobs = async (req,res) => {
+    const { name } = req.body;
 
-export default {getAllJobs}
+    const foundedJobs = await jobModel.findOne({ name });
+
+    if (foundedJobs) {
+        return res.status(409).send({
+            message: `Job: ${name} allaqachon mavjud`,
+        });
+    }
+
+    const job = await jobModel.create({ name })
+
+    res.send({
+        message: "success",
+        data: job
+    })
+};
+
+const updateJobs = async (req,res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if(!(id)){
+        return res.status(400).send({
+            message: `Given id: ${id} is not valid`
+        });
+    };
+
+    const job = await jobModel.findByIdAndUpdate(id, { name }, { new: true });
+
+    if(!job){
+        return res.status(404).send({
+            message: `Job with id: ${id} not found`
+        });
+    };
+
+    res.send({
+        message: "success",
+        data: job
+    });
+};
+
+const deleteJobs = async (req,res) => {
+    const { id } = req.params;
+
+    if(!(id)){
+        return res.status(400).send({
+            message: `Given id: ${id} is not valid`
+        });
+    };
+
+    const job = await jobModel.findByIdAndDelete(id);
+
+    if(!job){
+        return res.status(404).send({
+            message: `Job with id: ${id} not found`
+        });
+    };
+
+    res.send({
+        message: "success",
+        data: job
+    });
+}
+
+export default { getAllJobs, createJobs, updateJobs, deleteJobs, getOneJobs }; 
--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -3,6 +3,11 @@ import jobboardController from "../controller/jobboard.controller.js";
 
 const jobRouter = Router();
 
-jobRouter.get("/",jobboardController.getAllJobs);
+jobRouter
+    .get("/", jobboardController.getAllJobs)
+    .get("/:id", jobboardController.getOneJobs)
+    .post("/", jobboardController.createJobs)
+    .put("/:id", jobboardController.updateJobs)
+    .delete("/:id", jobboardController.deleteJobs)
 
 export default jobRouter;
