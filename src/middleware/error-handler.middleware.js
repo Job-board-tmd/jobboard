530bfb785ed471327f19396b87e08dd24759a390
muhiddin80 <rahimovmuhifdin@gmail.com>
2025-04-10 13:07:00 +0500

    halo

--- src/middleware/error-handler.middleware.js
+++ src/middleware/error-handler.middleware.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import logger from "../config/winston.config.js";
+import logger from "../config/winston.config.js"
 
-export const ErrorHandlerMiddleware = (err, _, res, __) => {
-  logger.error(err)
-=======
 function dublicateFieldError(err) {
   if (err?.code === 11000) {
     err.status = 409;
@@ -19,15 +15,17 @@ function dublicateFieldError(err) {
 
 
 export const ErrorHandlerMiddleware = (err, _, res, __) => {
+  logger.error(err)
+
+
   err = dublicateFieldError(err);
 
->>>>>>> muhiddin
   if (err.isException) {
     return res.status(err.status).send({
       message: err.message,
     });
   }
-// console.log(err)
+
   res.status(500).send({
     message: err,
   });
